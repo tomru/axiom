f200f709316ac8d33625a35786525d14c8207b50
Harry Hogg <harry@hogg.io>
2016-08-15 13:46:46 +0100

    Added label to text input, fixed input type and allowed variable icon sizing

--- src/components/form/TextInput.js
+++ src/components/form/TextInput.js
@@ -3,6 +3,7 @@ import classnames from 'classnames';
 import { enhance, addPropTypes, addClassName, findComponent } from '../_utils/components';
 import Button from '../button/Button';
 import Icon from '../icon/Icon';
+import Heading from '../typography/Heading';
 
 if (__INCLUDE_CSS__) {
   require('./TextInput.scss');
@@ -12,6 +13,7 @@ export class TextInput extends Component {
   static propTypes = {
     children: { node: true },
     invalid: { bool: true },
+    label: { string: true },
     thinking: { bool: true },
     valid: { bool: true },
   };
@@ -27,7 +29,7 @@ export class TextInput extends Component {
   }
 
   render() {
-    const { className, children, valid, invalid, thinking, ...rest } = this.props;
+    const { className, children, valid, invalid, label, thinking, ...rest } = this.props;
     const button = findComponent(children, Button);
     const icon = findComponent(children, Icon);
     const classes = classnames(className,
@@ -39,11 +41,17 @@ export class TextInput extends Component {
 
     return (
       <label className={ classes }>
+        { do { if (label) {
+          <Heading level={ 5 }>{ label }</Heading>
+        } } }
+
         <div className="ax-input__button-container">
           <div className="ax-input__icon-container">
             <input
-                className="ax-input" { ...rest }
-                ref="input"  />
+                { ...rest }
+                className="ax-input"
+                ref="input"
+                type="text"  />
 
             { do {
               if (icon) {
@@ -51,7 +59,6 @@ export class TextInput extends Component {
                   { cloneElement(icon, {
                     className: classnames({ 'ax-icon-spin': thinking }),
                     name: this.getIcon(icon.props.name),
-                    size: 'medium',
                   }) }
                 </div>
               }
