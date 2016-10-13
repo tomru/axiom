737b9481b54fe0127602762d8c51b7c56fce00fb
Harry Hogg <harry@hogg.io>
2016-10-13 09:20:29 +0100

    Removed some left over search bits

--- style-guide/actions/routing.js
+++ style-guide/actions/routing.js
@@ -1,13 +1,5 @@
 import { browserHistory } from 'react-router';
-import * as routes from 'style-guide/constants/Routing';
 import { getFirstPath } from 'style-guide/utils/examples';
-
-export function goToSearchPage(q) {
-  browserHistory.push({
-    pathname: `/${routes.SEARCH}`,
-    query: { q },
-  });
-}
 
 export function goToDocs() {
   browserHistory.push({
