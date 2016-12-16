57ad6e0c00469c427d4c9c0c2386c6c1a100ffa5
Sammy Braun <siggysamson@users.noreply.github.com>
2016-12-16 15:27:49 +0100

    Moved gh-pages deploy scripts to separate folder (#44)

--- deploy/ssh.sh
+++ deploy/ssh.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
-openssl aes-256-cbc -K $encrypted_5890ad0da7e1_key -iv $encrypted_5890ad0da7e1_iv -in publish-key.enc -out publish-key -d
-chmod 600 publish-key
+openssl aes-256-cbc -K $encrypted_5890ad0da7e1_key -iv $encrypted_5890ad0da7e1_iv -in ./deploy/publish-key.enc -out ./deploy/publish-key -d
+chmod 600 ./deploy/publish-key
 eval "$(ssh-agent -s)"
-ssh-add publish-key
+ssh-add ./deploy/publish-key
