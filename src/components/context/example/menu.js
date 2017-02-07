57af8baa66d567e78e3245cbb38056a9b9838e71
Harry Hogg <harry@hogg.io>
2017-02-07 16:17:15 +0000

    fix(ContextBox): When arrow is vertically positioned spacing is added to top and bottom

--- src/components/context/example/menu.js
+++ src/components/context/example/menu.js
@@ -1,49 +1,45 @@
 import React, { Component } from 'react';
 import { Example, Snippet } from 'style-guide';
-import { ContextMenu, ContextMenuItem, Grid, GridCell, Icon } from 'bw-axiom';
+import { ContextMenu, ContextMenuItem, Grid, GridCell } from 'bw-axiom';
 
 export default class ContextMenuExample extends Component {
   render() {
     return (
       <Example name="Menu">
         <Snippet>
-          <ContextMenu position="bottom">
-            <ContextMenuItem>
-              <Grid snippetReplace={ true } vAlign="middle">
-                <GridCell shrink={ true }>
-                  <Icon name="twitter" />
-                </GridCell>
-
-                <GridCell>
+          <Grid snippetIgnore={ true }>
+            <GridCell shrink={ true } snippetIgnore={ true }>
+              <ContextMenu position="bottom">
+                <ContextMenuItem>
                   Nam dignissim bibendum
-                </GridCell>
-              </Grid>
-            </ContextMenuItem>
+                </ContextMenuItem>
 
-            <ContextMenuItem>
-              <Grid snippetReplace={ true } vAlign="middle">
-                <GridCell shrink={ true }>
-                  <Icon name="twitter" />
-                </GridCell>
-
-                <GridCell>
+                <ContextMenuItem>
                   Nam dignissim bibendum
-                </GridCell>
-              </Grid>
-            </ContextMenuItem>
+                </ContextMenuItem>
 
-            <ContextMenuItem>
-              <Grid snippetReplace={ true } vAlign="middle">
-                <GridCell shrink={ true }>
-                  <Icon name="twitter" />
-                </GridCell>
-
-                <GridCell>
+                <ContextMenuItem>
                   Nam dignissim bibendum
-                </GridCell>
-              </Grid>
-            </ContextMenuItem>
-          </ContextMenu>
+                </ContextMenuItem>
+              </ContextMenu>
+            </GridCell>
+
+            <GridCell shrink={ true } snippetIgnore={ true }>
+              <ContextMenu position="right">
+                <ContextMenuItem>
+                  Nam dignissim bibendum
+                </ContextMenuItem>
+
+                <ContextMenuItem>
+                  Nam dignissim bibendum
+                </ContextMenuItem>
+
+                <ContextMenuItem>
+                  Nam dignissim bibendum
+                </ContextMenuItem>
+              </ContextMenu>
+            </GridCell>
+          </Grid>
         </Snippet>
       </Example>
     );
