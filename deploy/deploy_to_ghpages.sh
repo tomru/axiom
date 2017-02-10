d653d12191cd0758d932db1c9a96b037ad27e94a
Harry Hogg <harry@hogg.io>
2017-02-10 13:58:12 +0000

    chore(Travis): Fix gh-pages deploy from travis

--- deploy/deploy_to_ghpages.sh
+++ deploy/deploy_to_ghpages.sh
@@ -6,11 +6,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
-openssl aes-256-cbc -K $encrypted_5890ad0da7e1_key -iv $encrypted_5890ad0da7e1_iv -in ./deploy/publish-key.enc -out ./deploy/publish-key -d
-chmod 600 ./deploy/publish-key
-eval "$(ssh-agent -s)"
-ssh-add ./deploy/publish-key
-
 git config --global user.email "$COMMIT_AUTHOR_EMAIL"
 git config --global user.name "$COMMIT_AUTHOR_NAME"
 
