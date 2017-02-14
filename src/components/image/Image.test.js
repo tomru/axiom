1035af88b9140286268ece81d7789a066d317109
Harry Hogg <harry@hogg.io>
2017-02-14 10:00:17 +0000

    test(Image): Missing template tests

---
+++ src/components/image/Image.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Image } from 'bw-axiom';
+
+function getComponent(props = {}) {
+  return renderer.create(
+    <Image src="/image.png" { ...props } />
+  );
+}
+
+describe('Image', () => {
+  it('renders with defaultProps', () => {
+    const component = getComponent();
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders with extra props', () => {
+    const component = getComponent({ title: 'Lorem', alt: 'Lorem' });
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
