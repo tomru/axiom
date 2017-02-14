a656d6e1331dec6ac1a45b2613d70dad58728608
Harry Hogg <harry@hogg.io>
2017-02-14 11:39:20 +0000

    fix(Candytar): Block spacing and class rename

--- src/components/avatar/Candytar.js
+++ src/components/avatar/Candytar.js
@@ -57,9 +57,9 @@ export default class Candytar extends Component {
     /* eslint-disable max-len */
     return (
       <svg className={ classes } style={ style } viewBox="0 0 32 32">
-        <circle className="ax-candytar--background" cx="16" cy="16" r="16"></circle>
+        <circle className="ax-candytar__background" cx="16" cy="16" r="16"></circle>
         <path
-            className="ax-candytar--body"
+            className="ax-candytar__body"
             d="M16,20 C12.1340068,20 9,16.8659932 9,13 C9,9.13400675 12.1340068,6 16,6 C19.8659932,6 23,9.13400675 23,13 C23,16.8659932 19.8659932,20 16,20 Z M6,28.4693659 L6,28.4693659 C7.72660626,24.654283 11.5666365,22 16.0266078,22 C20.486579,22 24.3266093,24.654283 26.0532155,28.4693659 C23.3099448,30.6780366 19.8226569,32 16.0266078,32 C12.2305587,32 8.74327068,30.6780366 6,28.4693659 Z"
             filter="url(#ax-candytar__filter)"></path>
       </svg>
