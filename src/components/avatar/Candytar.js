5911dcf5de16d969cd5de962cb4cf8fea75bb264
Harry Hogg <harry@hogg.io>
2017-02-15 15:25:09 +0000

    fix(Candytars): Using one single filter

--- src/components/avatar/Candytar.js
+++ src/components/avatar/Candytar.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
+import './_filter';
 
 if (__INCLUDE_CSS__) {
   require('./Candytar.scss');
@@ -51,22 +52,16 @@ export default class Candytar extends Component {
   render() {
     const { picker, color = picker(colors), size } = this.props;
     const style = { height: size, width: size };
-    const classes = classnames('ax-candytar-placeholder', `ax-candytar-placeholder--${color}`);
-    const bodyColorMatrix = `
-      1 0 0 0    -0.27
-      0 1 0 0    -0.27
-      0 0 1 0    -0.27
-      0 0 0 0.55  0
-    `;
+    const classes = classnames('ax-candytar', `ax-candytar--${color}`);
 
     /* eslint-disable max-len */
     return (
       <svg className={ classes } style={ style } viewBox="0 0 32 32">
-        <filter id="ax-candytar-placeholder__filter">
-          <feColorMatrix type="matrix" values={ bodyColorMatrix }/>
-        </filter>
-        <circle className="ax-candytar-placeholder--background" cx="16" cy="16" r="16"></circle>
-        <path className="ax-candytar-placeholder--body" d="M16,20 C12.1340068,20 9,16.8659932 9,13 C9,9.13400675 12.1340068,6 16,6 C19.8659932,6 23,9.13400675 23,13 C23,16.8659932 19.8659932,20 16,20 Z M6,28.4693659 L6,28.4693659 C7.72660626,24.654283 11.5666365,22 16.0266078,22 C20.486579,22 24.3266093,24.654283 26.0532155,28.4693659 C23.3099448,30.6780366 19.8226569,32 16.0266078,32 C12.2305587,32 8.74327068,30.6780366 6,28.4693659 Z"></path>
+        <circle className="ax-candytar--background" cx="16" cy="16" r="16"></circle>
+        <path
+            className="ax-candytar--body"
+            d="M16,20 C12.1340068,20 9,16.8659932 9,13 C9,9.13400675 12.1340068,6 16,6 C19.8659932,6 23,9.13400675 23,13 C23,16.8659932 19.8659932,20 16,20 Z M6,28.4693659 L6,28.4693659 C7.72660626,24.654283 11.5666365,22 16.0266078,22 C20.486579,22 24.3266093,24.654283 26.0532155,28.4693659 C23.3099448,30.6780366 19.8226569,32 16.0266078,32 C12.2305587,32 8.74327068,30.6780366 6,28.4693659 Z"
+            filter="url(#ax-candytar__filter)"></path>
       </svg>
     );
     /* eslint-enable max-len */
---
+++ src/components/avatar/_filter.js
@@ -0,0 +1,10 @@
+import { svgDefineOnce } from 'bw-axiom';
+
+svgDefineOnce(`
+  <filter id="ax-candytar__filter">
+    <feColorMatrix type="matrix" values="1 0 0 0    -0.27
+                                         0 1 0 0    -0.27
+                                         0 0 1 0    -0.27
+                                         0 0 0 0.55  0" />
+  </filter>
+`);
