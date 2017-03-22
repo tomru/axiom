4ee6b5adb213e1c2cd1a41ef50257bacf72942ee
Harry Hogg <harry@hogg.io>
2017-03-22 12:57:17 +0000

    fix(IconButton): Swaps over IconButton to use circular Button

--- src/components/icon/IconButton.js
+++ src/components/icon/IconButton.js
@@ -1,10 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import classnames from 'classnames';
-import { Base, Icon } from 'bw-axiom';
-
-if (__INCLUDE_CSS__) {
-  require('./IconButton.scss');
-}
+import { Button, Icon } from 'bw-axiom';
 
 export default class IconButton extends Component {
   static propTypes = {
@@ -18,10 +13,6 @@ export default class IconButton extends Component {
 
   render() {
     const { name, size, ...rest } = this.props;
-    const classes = classnames('ax-icon-button', {
-      [`ax-icon-button--${size}`]: size,
-    });
-
     const sizeMap = {
       'small': '1rem',
       'medium': '2rem',
@@ -30,9 +21,9 @@ export default class IconButton extends Component {
     };
 
     return (
-      <Base Component="button" { ...rest } className={ classes }>
+      <Button style="secondary" { ...rest } circular={ size }>
         <Icon name={ name } size={ sizeMap[size] } />
-      </Base>
+      </Button>
     );
   }
 }
