8ea432840bbd9a15acf64729113cdea53de953be
Harry Hogg <harry@hogg.io>
2017-06-19 17:51:09 +0100

    docs(Alert): Swapped over to use configurable example

--- src/components/alert/example/index.js
+++ src/components/alert/example/index.js
@@ -1,5 +1,48 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Alert, Grid, GridCell, IconIndicator } from 'bw-axiom';
+import { ExampleConfig } from 'style-guide';
+
+class AlertExample extends Component {
+  static propTypes = {
+    components: PropTypes.shape({
+      Alert: PropTypes.object.isRequired,
+    }).isRequired,
+  };
+
+  render() {
+    const { components } = this.props;
+
+    const propTypes = {
+      Alert: components.Alert,
+    };
+
+    const initialProps = {
+      Alert: {
+        onRemoveClick: () => {},
+      },
+    };
+
+    return (
+      <ExampleConfig initialProps={ initialProps } propTypes={ propTypes }>
+        <Alert { ...initialProps.Alert }>
+          <Grid gutters="small" snippetReplace={ true } verticalAlign="middle">
+            <GridCell shrink={ true }>
+              <IconIndicator color="subtle" name="warning" />
+            </GridCell>
+
+            <GridCell>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac
+              accumsan quam, ut ullamcorper nulla.
+            </GridCell>
+          </Grid>
+        </Alert>
+      </ExampleConfig>
+    );
+  }
+}
+
+
 module.exports = [
-  require('./colors').default,
-  require('./removable').default,
-  require('./icon').default,
+  AlertExample,
 ];
