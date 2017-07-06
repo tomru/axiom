1655a1e50156980037968fae5788c39b18a91f0d
Harry Hogg <harry@hogg.io>
2017-07-06 11:35:52 +0100

    fix(MirroredColumnChart): Passing ColumnChart configuration props

--- src/charts/column-chart/MirroredColumnChart.js
+++ src/charts/column-chart/MirroredColumnChart.js
@@ -8,28 +8,55 @@ import './MirroredColumnChart.css';
 
 export default class MirroredColumnChart extends Component {
   static propTypes = {
+    /**
+     * Contextual component that appears when clicking on a bar.
+     * It is provided with the `color`, `label`, and `value` that has
+     * been clicked on, along with the `data` that was given for that row.
+     */
     ContextComponent: PropTypes.func,
+    /**
+     * The key that is shown along the bottom of the axis. It is also used
+     * to determine the order of bars.
+     */
     chartKey: PropTypes.arrayOf(PropTypes.shape({
       color: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
     })).isRequired,
+    /**
+     * The data used to render the bars along the top. The `label` is used for
+     * the xAxis and `values` for bars
+     */
     data: PropTypes.arrayOf(PropTypes.shape({
       label: PropTypes.node.isRequired,
       values: PropTypes.object.isRequired,
     })).isRequired,
+    /** Overall height of the individual column charts */
     height: PropTypes.string.isRequired,
+    /** Label to show on the top yAxis row */
     label: PropTypes.node,
+    /** The width of the yAxis labels columns */
     labelColumnWidth: PropTypes.string,
+    /**
+     * The data used to render the bars along the bottom. The `label` is used for
+     * the xAxis and `values` for bars
+     */
     reflectionData: PropTypes.arrayOf(PropTypes.shape({
       label: PropTypes.node.isRequired,
       values: PropTypes.object.isRequired,
     })).isRequired,
+    /** Label to show on the bottom yAxis row */
     reflectionLabel: PropTypes.node.isRequired,
-    showLabel: PropTypes.bool,
+    /** Control for showing the percentage labels above the Bars */
+    showBarLabel: PropTypes.bool,
+    /** Control for showing the labels below the Bars */
+    showSubLabel: PropTypes.bool,
+    /** Control for showing the xAxis labels */
+    showXAxisLabels: PropTypes.bool,
   };
 
   static defaultProps = {
     labelColumnWidth: '0rem',
+    showXAxisLabels: true,
   };
 
   render() {
@@ -42,6 +69,9 @@ export default class MirroredColumnChart extends Component {
       height,
       labelColumnWidth,
       reflectionData,
+      showBarLabel,
+      showSubLabel,
+      showXAxisLabels,
       ...rest
     } = this.props;
 
@@ -55,15 +85,20 @@ export default class MirroredColumnChart extends Component {
             direction="up"
             height={ height }
             label={ label }
-            labelColumnWidth={ labelColumnWidth }/>
+            labelColumnWidth={ labelColumnWidth }
+            showBarLabel={ showBarLabel }
+            showSubLabel={ showSubLabel }
+            showXAxisLabels={ false } />
 
-        <ColumnChartXAxis labelColumnWidth={ labelColumnWidth }>
-          { data.map(({ label }, index) =>
-            <ColumnChartXAxisLabel key={ index }>
-              { label }
-            </ColumnChartXAxisLabel>
-          ) }
-        </ColumnChartXAxis>
+        { showXAxisLabels && (
+          <ColumnChartXAxis labelColumnWidth={ labelColumnWidth }>
+            { data.map(({ label }, index) =>
+              <ColumnChartXAxisLabel key={ index }>
+                { label }
+              </ColumnChartXAxisLabel>
+            ) }
+          </ColumnChartXAxis>
+        ) }
 
         <ColumnChart
             ContextComponent={ ContextComponent }
@@ -72,7 +107,10 @@ export default class MirroredColumnChart extends Component {
             direction="down"
             height={ height }
             label={ reflectionLabel }
-            labelColumnWidth={ labelColumnWidth }/>
+            labelColumnWidth={ labelColumnWidth }
+            showBarLabel={ showBarLabel }
+            showSubLabel={ showSubLabel }
+            showXAxisLabels={ false } />
       </Base>
     );
   }
