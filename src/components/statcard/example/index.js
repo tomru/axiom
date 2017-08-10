b19eef80acee6ac15e9bb066b9ea74758dafb107
Harry Hogg <harry@hogg.io>
2017-08-10 17:40:33 +0100

    docs(StatCards): Moved over to use ExampleConfig

--- src/components/statcard/example/index.js
+++ src/components/statcard/example/index.js
@@ -1,6 +1,71 @@
-module.exports = [
-  require('./single-card').default,
-  require('./multiple-cards').default,
-  require('./cards-with-help').default,
-  require('./stat-colors').default,
-];
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { ExampleConfig } from 'style-guide';
+import { StatCards, StatCard, StatHeading, Stats, Stat, tinyNumber } from 'bw-axiom';
+
+function randomStatNumber(l = 30000, u = 999999) {
+  return tinyNumber(Math.floor(Math.random() * (u - l + 1) + l), 1);
+}
+
+class StatCardsExample extends Component {
+  static propTypes = {
+    components: PropTypes.shape({
+      StatCards: PropTypes.object,
+    }).isRequired,
+  };
+
+  render() {
+    const { components } = this.props;
+    const propTypes = {
+      StatCards: components.StatCards,
+      StatCard: components.StatCard,
+      StatHeading: components.StatHeading,
+      Stats: components.Stats,
+      Stat: components.Stat,
+    };
+
+    const initialProps = {
+      StatCards: {},
+      StatCard: {},
+      StatHeading: {},
+      Stats: {},
+      Stat: {},
+    };
+
+    const initialPropOptions = {
+      StatCards: {
+        children: {
+          count: 3,
+          min: 1,
+          max: Infinity,
+        },
+      },
+      Stat: {
+        color: {
+          applyToIndex: 0,
+        },
+      },
+    };
+
+    return (
+      <ExampleConfig
+          initialPropOptions={ initialPropOptions }
+          initialProps={ initialProps }
+          propTypes={ propTypes }>
+        <StatCards { ...initialProps.StatCards }>
+          <StatCard { ...initialProps.StatCard }>
+            <StatHeading { ...initialProps.StatHeading }>
+              Lorem Ipsum
+            </StatHeading>
+            <Stats { ...initialProps.Stats }>
+              <Stat color="blue" { ...initialProps.Stat }>{ randomStatNumber() }</Stat>
+              <Stat { ...initialProps.Stat }>{ randomStatNumber() }</Stat>
+            </Stats>
+          </StatCard>
+        </StatCards>
+      </ExampleConfig>
+    );
+  }
+}
+
+module.exports = [ StatCardsExample ];
--- src/components/statcard/example/utils.js
+++
@@ -1,5 +0,0 @@
-import { tinyNumber } from 'bw-axiom';
-
-export function randomStatNumber(l = 30000, u = 999999) {
-  return tinyNumber(Math.floor(Math.random() * (u - l + 1) + l), 1);
-}
