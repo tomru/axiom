50cbcafcd34f31aff8f895ec5bcf5867770d03a8
Benji Hall <benji@brandwatch.com>
2017-09-08 16:28:12 +0100

    chore(build): Add an ES modules build with a `module` field in the package.json to enable tree-shaking with Webpack

--- deploy/travis_setup.sh
+++ deploy/travis_setup.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-rm -rf tmp
+rm -rf esm tmp
 mkdir tmp
 rsync -a --prune-empty-dirs --exclude 'example/*' src/* tmp
+cp -r tmp esm
+
 NODE_ENV=production BABEL_ENV=production node_modules/.bin/babel tmp --out-dir tmp
+
+NODE_ENV=production BABEL_ENV=production-esm node_modules/.bin/babel esm --out-dir esm
