224a8fb464a7e8753d9fffbe229891eac1b00d0a
Harry Hogg <harry@hogg.io>
2017-09-12 10:19:50 +0100

    feat(Alert): Adds a new 'size' property

--- src/components/alert/Alert.js
+++ src/components/alert/Alert.js
@@ -13,18 +13,24 @@ import './Alert.css';
 
 export default class Alert extends Component {
   static propTypes = {
+    /** Content displayed next to the AlertIcon */
     children: PropTypes.node.isRequired,
+    /** Size of the Alert */
+    size: PropTypes.oneOf(['small', 'medium']),
+    /** Type of Alert that affects the coloring and icon */
     type: PropTypes.oneOf(['success', 'warning', 'error', 'info']),
+    /** An optional callback that when given adds a removable cross */
     onRemoveClick: PropTypes.func,
   };
 
   static defaultProps = {
+    size: 'small',
     type: 'info',
   };
 
   render() {
-    const { children, type, onRemoveClick, ...rest } = this.props;
-    const classes = classnames('ax-alert', `ax-alert--${type}`);
+    const { children, size, type, onRemoveClick, ...rest } = this.props;
+    const classes = classnames('ax-alert', `ax-alert--${size}`, `ax-alert--${type}`);
 
     return (
       <Base { ...rest } className={ classes } theme="light">
