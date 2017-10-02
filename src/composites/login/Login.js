e8a3f4c861d9d344e66d660c55f7eeebea581780
Harry Hogg <harry@hogg.io>
2017-10-02 16:10:43 +0100

    chore(Login): Swapped over Login composite to use LogoPage composite

--- src/composites/login/Login.js
+++ src/composites/login/Login.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import omit from 'lodash.omit';
-import Base from '../../components/base/Base';
 import Button from '../../components/button/Button';
 import ButtonGroup from '../../components/button/ButtonGroup';
 import Form from '../../components/form/Form';
@@ -9,7 +8,7 @@ import Grid from '../../components/grid/Grid';
 import GridCell from '../../components/grid/GridCell';
 import Heading from '../../components/typography/Heading';
 import Link from '../../components/typography/Link';
-import LogoTab from '../../components/logo/LogoTab';
+import LogoPage from '../logo-page/LogoPage';
 import Paragraph from '../../components/typography/Paragraph';
 import Strong from '../../components/typography/Strong';
 import TextInput from '../../components/form/TextInput';
@@ -18,10 +17,6 @@ import t from '../../utils/locales';
 import './Login.css';
 
 export default class Login extends Component {
-  static contextTypes = {
-    axiomLanguage: PropTypes.string,
-  };
-
   static propTypes = {
     /** Name of the application that appears in the header */
     application: PropTypes.string.isRequired,
@@ -29,8 +24,14 @@ export default class Login extends Component {
     backgroundImage: PropTypes.string.isRequired,
     /** An error message that will be placed on the page */
     error: PropTypes.string,
+    /** Overall theme for the page, needs to be set when using a background image */
+    theme: PropTypes.oneOf(['dark', 'light']).isRequired,
     /** Submit handler that will be called with the username and password */
     onSubmit: PropTypes.func.isRequired,
+  };
+
+  static contextTypes = {
+    axiomLanguage: PropTypes.string,
   };
 
   componentWillMount() {
@@ -57,76 +58,65 @@ export default class Login extends Component {
   }
 
   render() {
-    const { application, backgroundImage, error, ...rest } = this.props;
+    const { application, error, ...rest } = this.props;
     const { username, password } = this.state;
     const { axiomLanguage } = this.context;
-    const style = { backgroundImage: `url(${backgroundImage})` };
 
     return (
-      <Base { ...omit(rest, ['onSubmit']) }
-          className="ax-login"
-          style={ style }>
-        <div className="ax-login__header">
-          <div className="ax-login__header-container">
-            <LogoTab color="grey" height="7.75rem" />
-          </div>
+      <LogoPage { ...omit(rest, ['onSubmit']) }>
+        <div className="ax-login">
+          <Heading space="x4" textSize="display2">
+            <Strong>{ application }</Strong>/ { t(axiomLanguage, 'login-title') }
+          </Heading>
+
+          <Form onSubmit={ e => this.handleSubmit(e) }>
+            <TextInput
+                data-ax-at={ atIds.Login.username }
+                onChange={ e => this.handleUsernameChange(e) }
+                placeholder={ t(axiomLanguage, 'username') }
+                size="large"
+                style="overlay"
+                value={ username } />
+
+            <TextInput
+                data-ax-at={ atIds.Login.password }
+                onChange={ e => this.handlePasswordChange(e) }
+                placeholder={ t(axiomLanguage, 'password') }
+                size="large"
+                style="overlay"
+                type="password"
+                value={ password } />
+
+            <Grid space="x4" verticalAlign="middle">
+              <GridCell shrink>
+                <ButtonGroup>
+                  <Button
+                      data-ax-at={ atIds.Login.submit }
+                      full="small"
+                      size="large"
+                      type="submit">{ t(axiomLanguage, 'login-button') }</Button>
+                </ButtonGroup>
+              </GridCell>
+
+              <GridCell>
+                <Paragraph textCenter textRight="small">
+                  <Link href="https://app.brandwatch.com/login" target="_blank">
+                    <Strong>{ t(axiomLanguage, 'forgot-login') }</Strong>
+                  </Link>
+                </Paragraph>
+              </GridCell>
+            </Grid>
+          </Form>
+
+          { error && (
+            <Paragraph space="x4" textColor="error">
+              <Strong data-ax-at={ atIds.Login.error }>
+                { error }
+              </Strong>
+            </Paragraph>
+          ) }
         </div>
-
-        <div className="ax-login__body">
-          <div className="ax-login__form">
-            <Heading space="x4" textSize="display2">
-              <Strong>{ application }</Strong>/ { t(axiomLanguage, 'login-title') }
-            </Heading>
-
-            <Form onSubmit={ e => this.handleSubmit(e) }>
-              <TextInput
-                  data-ax-at={ atIds.Login.username }
-                  onChange={ e => this.handleUsernameChange(e) }
-                  placeholder={ t(axiomLanguage, 'username') }
-                  size="large"
-                  style="overlay"
-                  value={ username } />
-
-              <TextInput
-                  data-ax-at={ atIds.Login.password }
-                  onChange={ e => this.handlePasswordChange(e) }
-                  placeholder={ t(axiomLanguage, 'password') }
-                  size="large"
-                  style="overlay"
-                  type="password"
-                  value={ password } />
-
-              <Grid space="x4" verticalAlign="middle">
-                <GridCell shrink>
-                  <ButtonGroup>
-                    <Button
-                        data-ax-at={ atIds.Login.submit }
-                        full="small"
-                        size="large"
-                        type="submit">{ t(axiomLanguage, 'login-button') }</Button>
-                  </ButtonGroup>
-                </GridCell>
-
-                <GridCell>
-                  <Paragraph textCenter textRight="small">
-                    <Link href="https://app.brandwatch.com/login" target="_blank">
-                      <Strong>{ t(axiomLanguage, 'forgot-login') }</Strong>
-                    </Link>
-                  </Paragraph>
-                </GridCell>
-              </Grid>
-            </Form>
-
-            { error && (
-              <Paragraph space="x4" textColor="error">
-                <Strong data-ax-at={ atIds.Login.error }>
-                  { error }
-                </Strong>
-              </Paragraph>
-            ) }
-          </div>
-        </div>
-      </Base>
+      </LogoPage>
     );
   }
 }
