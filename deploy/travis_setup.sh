98aea4fc8f4b2c0375c7cf3c7bcb3ebf6e3de159
Harry Hogg <harry@hogg.io>
2017-10-03 21:27:31 +0100

    chore(ESM): Removed ESM build

--- deploy/travis_setup.sh
+++ deploy/travis_setup.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-rm -rf esm tmp
+rm -rf tmp
 mkdir tmp
 rsync -a --prune-empty-dirs --exclude 'example/*' src/* tmp
-cp -r tmp esm
 
 NODE_ENV=production BABEL_ENV=production node_modules/.bin/babel tmp --out-dir tmp
-
-NODE_ENV=production BABEL_ENV=production-esm node_modules/.bin/babel esm --out-dir esm
