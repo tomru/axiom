13c368507ad3c80aa2ad4ca89d74ea9e65e84b3b
TomWija <tomwi@brandwatch.com>
2017-10-09 12:16:56 +0100

    feat(canvas): add 'hasDock' prop Change to make canvas look right without a dock present. Remove the whitespace present when the dock is not included

--- src/components/platform/Canvas.js
+++ src/components/platform/Canvas.js
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Base from '../base/Base';
+import classnames from 'classnames';
 import './Canvas.css';
 
 export default class Canvas extends Component {
   static propTypes = {
     children: PropTypes.node,
+    isDockless: PropTypes.bool,
     isShifted: PropTypes.bool,
   };
 
@@ -14,15 +16,18 @@ export default class Canvas extends Component {
   };
 
   render() {
-    const { children, isShifted, ...rest } = this.props;
+    const { children, isDockless, isShifted, ...rest } = this.props;
     const { consoleWidth } = this.context;
     const style = {
       marginRight: isShifted && consoleWidth,
       transform: isShifted && `translateX(${consoleWidth})`,
     };
+    const classes = classnames('ax-platform__canvas', {
+      'ax-platform__canvas--no-dock': isDockless,
+    });
 
     return (
-      <Base { ...rest } className="ax-platform__canvas" style={ style }>
+      <Base { ...rest } className={ classes } style={ style }>
         { children }
       </Base>
     );
