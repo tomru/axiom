38f806241469ea333f3afbfa2dfc4ff6be677b55
Michael Sorhaindo <pmsorhaindo@gmail.com>
2017-11-21 12:49:57 +0000

    chore(enzyme): Prevent react 16 warnings in requestAnimationFrame tests

--- enzyme.js
+++ enzyme.js
@@ -1,3 +1,6 @@
+// Prevent React 16 warnings on the console during tests using requestAnimationFrame
+global.requestAnimationFrame = setTimeout;
+
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
