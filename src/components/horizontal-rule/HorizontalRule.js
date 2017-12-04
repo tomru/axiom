37fe464dd07a9d5ee9fcf9922d022e47bd00b0ad
Michael Sorhaindo <pmsorhaindo@gmail.com>
2017-12-04 17:31:53 +0000

    fix(HorizontalRule): make 0 margins overridable

--- src/components/horizontal-rule/HorizontalRule.js
+++ src/components/horizontal-rule/HorizontalRule.js
@@ -6,7 +6,7 @@ export default class HorizontalRule extends Component {
   render() {
     const { ...rest } = this.props;
     return (
-      <Base { ...rest } Component="hr" className="ax-horizontal-rule" />
+      <Base space="x0" { ...rest } Component="hr" className="ax-horizontal-rule" />
     );
   }
 }
