fa0446fa8c554ee76caf60cc3e2a5be59cb37400
thatguynamedandy <andyp@brandwatch.com>
2018-06-21 15:51:48 +0100

    fixup! feat(Tooltip): Adds boolean delay option

--- packages/axiom-components/src/Tooltip/TooltipTarget.js
+++ packages/axiom-components/src/Tooltip/TooltipTarget.js
@@ -18,7 +18,7 @@ export default class TooltipTarget extends Component {
 
   static typeRef = TooltipTargetRef;
 
-  handleMouseMove(event, ...args) {
+  handleMouseMove(...args) {
     const { children, delay } = this.props;
     const { showTooltip } = this.context;
     const { onMouseMove = () => {} } = children.props;
@@ -29,7 +29,7 @@ export default class TooltipTarget extends Component {
 
     if (!delay) showTooltip();
 
-    onMouseMove(event, ...args);
+    onMouseMove(...args);
   }
 
   handleMouseLeave(...args) {
