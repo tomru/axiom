8694bf03179fbc0e51602a4102b70e965c1fb986
Brandwatch (via TravisCI) <ci@brandwatch.com>
2018-07-03 12:14:09 +0200

    chore(CI): configure ssh key in git config

--- scripts/publish_packages.sh
+++ scripts/publish_packages.sh
@@ -6,9 +6,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
 fi
 
 openssl aes-256-cbc -K $encrypted_7c2951e40ae6_key -iv $encrypted_7c2951e40ae6_iv -in github_deploy_key.enc -out github_deploy_key -d
-chmod 600 github_deploy_key
-eval `ssh-agent -s`
-ssh-add github_deploy_key
 
 # Test SSH connection
 if ! ssh git@github.com 2>&1 | grep "BrandwatchLtd/axiom" > /dev/null
@@ -20,6 +17,7 @@ fi
 
 git config --global user.email "ci@brandwatch.com"
 git config --global user.name "Brandwatch (via TravisCI)"
+git config --global core.sshCommand "ssh -i github_deploy_key -F /dev/null"
 
 npm config set "//registry.npmjs.org/:_authToken=\${NPM_API_KEY}"
 
@@ -27,6 +25,3 @@ yarn build:packages
 
 git checkout master
 npx lerna publish --conventional-commits --yes
-
-# Workaround https://github.com/travis-ci/travis-ci/issues/8082
-ssh-agent -k
