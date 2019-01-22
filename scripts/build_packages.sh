d6d50e7c70260c7ac3c88702d5ec6146213b8b4a
Thomas Ruoff <ThomasRuoff@gmail.com>
2019-01-22 10:34:31 +0100

    chore(build): set babel envs in scripts

--- scripts/build_packages.sh
+++ scripts/build_packages.sh
@@ -3,6 +3,6 @@ set -e
 
 npx lerna exec --parallel -- rm -rf dist
 npx lerna exec --parallel -- rsync -a --prune-empty-dirs --exclude '__snapshots__/*' --exclude '*.test.js' src/* dist
-npx lerna exec --parallel -- npx babel dist -d dist
+npx lerna exec --parallel -- BABEL_ENV=production npx babel dist -d dist
 
 npx postcss packages/**/dist/**/*.css --config ./postcss.prod.config.js --replace --verbose
--- scripts/ci_setup.sh
+++ scripts/ci_setup.sh
@@ -3,4 +3,4 @@ set -e
 
 npx lerna exec --parallel -- rm -rf dist
 npx lerna exec --parallel -- rsync -a --prune-empty-dirs src/* dist
-npx lerna exec --parallel -- npx babel dist -d dist
+npx lerna exec --parallel -- BABEL_ENV=test npx babel dist -d dist
