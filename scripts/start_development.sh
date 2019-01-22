af244e6dbe0b707d47553f93602b5b0031af4fb8
Thomas Ruoff <ThomasRuoff@gmail.com>
2019-01-22 16:24:51 +0100

    chore(scripts): clean new dist dirs in start_development.sh

--- scripts/start_development.sh
+++ scripts/start_development.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-npx lerna exec --parallel -- "rm -rf dist && ln -s src dist"
+npx lerna exec --parallel -- "rm -rf dist-cjs dist-esm && ln -s src dist"
 npx webpack-dev-server
