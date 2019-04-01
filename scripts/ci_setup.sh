93915544b8e57f236e7d60ee63611ef0f70f3552
Thomas Ruoff <ThomasRuoff@gmail.com>
2019-04-01 13:04:31 +0200

    fix(ci): ci:setup and ci:test work again

--- scripts/ci_setup.sh
+++ scripts/ci_setup.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 set -e
-npx lerna exec --parallel -- rm -rf dist-cjs
-npx lerna exec --parallel -- rsync -a --prune-empty-dirs src/* dist-cjs
 
-export BABEL_ENV=test
-npx lerna exec --parallel -- npx babel dist-cjs -d dist-cjs
+DIST_CJS="dist-cjs"
+DIST_ESM="dist-esm"
+
+# Prepare
+npx lerna exec --parallel -- rm -rf $DIST_CJS $DIST_ESM dist
+
+# Build
+yarn build:flags
+
+# CommonJS build
+export BABEL_ENV=production-cjs
+
+npx lerna exec --parallel -- rsync -a --prune-empty-dirs src/* $DIST_CJS
+npx lerna exec --parallel -- npx babel $DIST_CJS -d $DIST_CJS
+npx postcss packages/**/$DIST_CJS/**/*.css --config ./postcss.prod.config.js --replace --verbose
+
+# ES Module build
+export BABEL_ENV=production-esm
+
+npx lerna exec --parallel -- rsync -a --prune-empty-dirs src/* $DIST_ESM
+npx lerna exec --parallel -- npx babel $DIST_ESM -d $DIST_ESM
+npx postcss packages/**/$DIST_ESM/**/*.css --config ./postcss.prod.config.js --replace --verbose
