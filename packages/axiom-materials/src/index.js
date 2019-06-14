e8f039f63b3180b86fd4335a70adfb2b50599145
Thomas Ruoff <ThomasRuoff@gmail.com>
2019-06-14 11:35:59 +0200

    feat(colorUtils): added colorUtils with rgb2hex converter

--- packages/axiom-materials/src/index.js
+++ packages/axiom-materials/src/index.js
@@ -1,8 +1,10 @@
 import * as colors from './colors';
+import * as colorUtils from './colorUtils';
 import * as opacities from './opacities';
 
 export { default as animations } from './animations';
 export { colors as colors };
+export { colorUtils as colorUtils };
 export { default as layout } from './layout';
 export { opacities as opacities };
 export { default as icons } from './icons';
