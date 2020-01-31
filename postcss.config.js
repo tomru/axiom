bfcaa79d5d4372cf18a73eccf86f3a2ae144e35f
laurie <laurie@brandwatch.com>
2020-01-31 17:08:38 +0000

    build(post-css): Upgrade Post CSS env

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,21 @@
-const cssCustomProperties = require('@brandwatch/axiom-materials/css-custom-properties');
-
 module.exports = {
   plugins: [
-    require('postcss-cssnext')({
-      browsers: [
-        ...require('@brandwatch/axiom-materials/browsers'),
-        'IE 11',
+    require('postcss-preset-env')({
+      stage: 1,
+      browsers: [...require('@brandwatch/axiom-materials/browsers', 'IE 11')],
+      importFrom: [
+        './packages/axiom-materials/src/animations.css',
+        './packages/axiom-materials/src/layout.css',
+        './packages/axiom-materials/src/colors.css',
+        './packages/axiom-materials/src/opacities.css',
+        './packages/axiom-materials/src/sizing.css',
+        './packages/axiom-materials/src/typography.css',
+        './packages/axiom-materials/src/theme-day.css',
+        './packages/axiom-materials/src/theme-night.css',
       ],
       features: {
         autoprefixer: {
           grid: true,
-        },
-        customProperties: {
-          preserve: true,
-          warnings: false,
-          variables: cssCustomProperties({ theme: 'day' }),
         },
       },
     }),
