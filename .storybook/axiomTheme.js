d992d054f580f2955ca6840a4619f30a28a9cce9
laurie <laurie@brandwatch.com>
2020-02-24 14:27:29 +0000

    feat: 🎸 Storybook-theme
    Adds minimal custom theme

---
+++ .storybook/axiomTheme.js
@@ -0,0 +1,6 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  brandTitle: 'Axiom',
+  brandUrl: 'https://axiom.brandwatch.com/',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import axiomTheme from './axiomTheme';
+
+addons.setConfig({
+  theme: axiomTheme,
+});
