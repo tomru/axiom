8e75699266f474226d5429000be90175e79b65bb
laurie <laurie@brandwatch.com>
2020-02-24 14:26:06 +0000

    feat: 🎸 Storybook
    Adds minimal example of a Story

---
+++ packages/axiom-components/src/Button/Button.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from './Button';
+
+export default {
+  title: 'Components | Button',
+  component: Button,
+};
+
+export function Default() {
+  return <Button>Default Button</Button>;
+}
