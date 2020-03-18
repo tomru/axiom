b015065a99150b8eec6c054937159d7cb43df694
laurie <laurie@brandwatch.com>
2020-03-18 16:14:35 +0000

    docs: ✏️ Tootip adds minimal example

---
+++ packages/axiom-components/src/Tooltip/Tooltip.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Tooltip from './Tooltip';
+import TooltipTarget from './TooltipTarget';
+import TooltipSource from './TooltipSource';
+import TooltipContext from './TooltipContext';
+import TooltipContent from './TooltipContent';
+import IconButton from '../Icon/IconButton';
+
+export default {
+  title: 'Components/Tooltip',
+  component: Tooltip,
+  subcomponents: { TooltipTarget, TooltipSource, TooltipContent, TooltipContext },
+};
+
+export function Default() {
+  return (
+    <Tooltip position="bottom">
+      <TooltipTarget>
+        <IconButton name="question-mark" size="small"/>
+      </TooltipTarget>
+
+      <TooltipSource width="auto">
+        <TooltipContext>
+          <TooltipContent size="tiny">50%</TooltipContent>
+        </TooltipContext>
+      </TooltipSource>
+    </Tooltip>
+  );
+}
