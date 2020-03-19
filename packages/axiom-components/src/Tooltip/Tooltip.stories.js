431f605baf0678b971c4d8ef85062f6661b3bbd7
laurie <laurie@brandwatch.com>
2020-03-19 15:06:39 +0000

    docs: ✏️ Tooltip adds and example of using a ref

--- packages/axiom-components/src/Tooltip/Tooltip.stories.js
+++ packages/axiom-components/src/Tooltip/Tooltip.stories.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Tooltip from './Tooltip';
 import TooltipTarget from './TooltipTarget';
 import TooltipSource from './TooltipSource';
 import TooltipContext from './TooltipContext';
 import TooltipContent from './TooltipContent';
 import IconButton from '../Icon/IconButton';
+import Button from '../Button/Button';
 
 export default {
   title: 'Components/Tooltip',
@@ -62,5 +63,38 @@ export function Delay() {
         </TooltipContext>
       </TooltipSource>
     </Tooltip>
+  );
+}
+
+export function WithRef() {
+  const ref = useRef();
+
+  return (
+    <React.Fragment>
+      <Button
+          onClick={ () => {
+            ref.current.showTooltip();
+          } }>
+          Open
+      </Button>
+      <Button
+          onClick={ () => {
+            ref.current.hideTooltip();
+          } }
+          style="secondary">
+          Close
+      </Button>
+      <Tooltip position="bottom" ref={ ref }>
+        <TooltipTarget>
+          <IconButton name="question-mark" size="small" />
+        </TooltipTarget>
+
+        <TooltipSource width="auto">
+          <TooltipContext>
+            <TooltipContent size="tiny">50%</TooltipContent>
+          </TooltipContext>
+        </TooltipSource>
+      </Tooltip>
+    </React.Fragment>
   );
 }
