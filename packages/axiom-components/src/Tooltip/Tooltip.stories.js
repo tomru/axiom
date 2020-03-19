0292fcf91956d9383aa5ba26b55df81ccf480827
laurie <laurie@brandwatch.com>
2020-03-19 11:10:31 +0000

    docs: ✏️ Tooltip adds stories for disabled and delay

--- packages/axiom-components/src/Tooltip/Tooltip.stories.js
+++ packages/axiom-components/src/Tooltip/Tooltip.stories.js
@@ -9,14 +9,51 @@ import IconButton from '../Icon/IconButton';
 export default {
   title: 'Components/Tooltip',
   component: Tooltip,
-  subcomponents: { TooltipTarget, TooltipSource, TooltipContent, TooltipContext },
+  subcomponents: {
+    TooltipTarget,
+    TooltipSource,
+    TooltipContent,
+    TooltipContext,
+  },
 };
 
 export function Default() {
   return (
     <Tooltip position="bottom">
       <TooltipTarget>
-        <IconButton name="question-mark" size="small"/>
+        <IconButton name="question-mark" size="small" />
+      </TooltipTarget>
+
+      <TooltipSource width="auto">
+        <TooltipContext>
+          <TooltipContent size="tiny">50%</TooltipContent>
+        </TooltipContext>
+      </TooltipSource>
+    </Tooltip>
+  );
+}
+
+export function Disabled() {
+  return (
+    <Tooltip enabled={ false } position="bottom">
+      <TooltipTarget>
+        <IconButton name="question-mark" size="small" />
+      </TooltipTarget>
+
+      <TooltipSource width="auto">
+        <TooltipContext>
+          <TooltipContent size="tiny">50%</TooltipContent>
+        </TooltipContext>
+      </TooltipSource>
+    </Tooltip>
+  );
+}
+
+export function Delay() {
+  return (
+    <Tooltip delay position="bottom">
+      <TooltipTarget>
+        <IconButton name="question-mark" size="small" />
       </TooltipTarget>
 
       <TooltipSource width="auto">
