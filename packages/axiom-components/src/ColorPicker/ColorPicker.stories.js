3063cce025ddf12999d8dfb4f07e9daca6f4e8ff
laurie <laurie@brandwatch.com>
2020-03-24 15:27:25 +0000

    docs: ✏️ ColorPicker adds basic example

---
+++ packages/axiom-components/src/ColorPicker/ColorPicker.stories.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+import React from 'react';
+import ColorPicker from './ColorPicker';
+
+export default {
+  title: 'Components/ColorPicker',
+  component: ColorPicker,
+};
+
+export function Default() {
+  function colorSelectCallback(color) {
+    console.log(`the color ${color} was selected`);
+  }
+
+  return <ColorPicker onSelectColor={ colorSelectCallback } />;
+}
