79296852ae37f7ab5c1d6a76c9b186a7e26a701d
laurie <laurie@brandwatch.com>
2020-03-24 15:28:05 +0000

    refactor: 💡 ColorPicker, convert to functional style

--- packages/axiom-components/src/ColorPicker/ColorPicker.js
+++ packages/axiom-components/src/ColorPicker/ColorPicker.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import atIds from '@brandwatch/axiom-automation-testing/ids';
 import Dropdown from '../Dropdown/Dropdown';
 import DropdownContent from '../Dropdown/DropdownContent';
@@ -10,170 +10,161 @@ import Grid from '../Grid/Grid';
 import GridCell from '../Grid/GridCell';
 import ColorPickerOption from './ColorPickerOption';
 
-export default class ColorPicker extends Component {
-  static propTypes = {
-    /** Colors that should be disabled from selection */
-    disabledOptions: PropTypes.arrayOf(PropTypes.oneOf([
-      'forbidden-planet',
-      'tiny-clanger',
-      'critical-mass',
-      'fantastic-voyage',
-      'paradise-lost',
-      'serene-sea',
-      'event-horizon',
-      'electric-dreams',
-      'outer-limits',
-      'giant-leap',
-      'moon-lagoon',
-      'space-invader',
-      'extraterrestrial',
-      'terra-form',
-      'primeval-soup',
-      'future-shock',
-      'sun-maker',
-      'new-horizon',
-      'blast-off',
-      'crash-course',
-      'solar-rust',
-      'ground-control',
-      'space-oddity',
-      'rocky-planet',
-      'deep-thought',
-      'luna-dust',
-    ])),
-    /** Callback for when the color picker has been opened */
-    onOpen: PropTypes.func,
-    /** Callback for when a color has been selected, provided with the selected color */
-    onSelectColor: PropTypes.func.isRequired,
-    /** Colors that should be visible for selection */
-    options: PropTypes.arrayOf(PropTypes.oneOf([
-      'forbidden-planet',
-      'tiny-clanger',
-      'critical-mass',
-      'fantastic-voyage',
-      'paradise-lost',
-      'serene-sea',
-      'event-horizon',
-      'electric-dreams',
-      'outer-limits',
-      'giant-leap',
-      'moon-lagoon',
-      'space-invader',
-      'extraterrestrial',
-      'terra-form',
-      'primeval-soup',
-      'future-shock',
-      'sun-maker',
-      'new-horizon',
-      'blast-off',
-      'crash-course',
-      'solar-rust',
-      'ground-control',
-      'space-oddity',
-      'rocky-planet',
-      'deep-thought',
-      'luna-dust',
-    ])),
-    /** Color that should appear as selected */
-    selected: PropTypes.oneOf([
-      'forbidden-planet',
-      'tiny-clanger',
-      'critical-mass',
-      'fantastic-voyage',
-      'paradise-lost',
-      'serene-sea',
-      'event-horizon',
-      'electric-dreams',
-      'outer-limits',
-      'giant-leap',
-      'moon-lagoon',
-      'space-invader',
-      'extraterrestrial',
-      'terra-form',
-      'primeval-soup',
-      'future-shock',
-      'sun-maker',
-      'new-horizon',
-      'blast-off',
-      'crash-course',
-      'solar-rust',
-      'ground-control',
-      'space-oddity',
-      'rocky-planet',
-      'deep-thought',
-      'luna-dust',
-    ]),
-    /** Diameter of the color picker button */
-    size : PropTypes.string,
-    /** Width of the color selection container */
-    width: PropTypes.string,
-  };
+export default function ColorPicker({
+  disabledOptions = [],
+  options = [
+    'tiny-clanger',
+    'critical-mass',
+    'paradise-lost',
+    'serene-sea',
+    'giant-leap',
+    'moon-lagoon',
+    'terra-form',
+    'primeval-soup',
+    'new-horizon',
+    'blast-off',
+    'ground-control',
+    'luna-dust',
+  ],
+  width = '9rem',
+  onOpen,
+  onSelectColor,
+  selected,
+  size,
+  ...rest
+}) {
+  return (
+    <Dropdown position="bottom" { ...rest } showArrow>
+      <DropdownTarget>
+        <ColorPickerOption
+            color={ selected }
+            data-ax-at={ atIds.ColorPicker.dropdown }
+            onClick={ onOpen }
+            size={ size }/>
+      </DropdownTarget>
 
-  static defaultProps = {
-    disabledOptions: [],
-    options: [
-      'tiny-clanger',
-      'critical-mass',
-      'paradise-lost',
-      'serene-sea',
-      'giant-leap',
-      'moon-lagoon',
-      'terra-form',
-      'primeval-soup',
-      'new-horizon',
-      'blast-off',
-      'ground-control',
-      'luna-dust',
-    ],
-    width: '9rem',
-  };
-
-  render() {
-    const {
-      disabledOptions,
-      onOpen,
-      onSelectColor,
-      options,
-      selected,
-      width,
-      size,
-      ...rest
-    } = this.props;
-
-    return (
-      <Dropdown position="bottom" { ... rest } showArrow>
-        <DropdownTarget>
-          <ColorPickerOption
-              color={ selected }
-              data-ax-at={ atIds.ColorPicker.dropdown }
-              onClick={ onOpen }
-              size={ size } />
-        </DropdownTarget>
-
-        <DropdownSource>
-          <DropdownContext width={ width }>
-            <DropdownContent>
-              <Grid
-                  gutters="small"
-                  horizontalAlign="middle"
-                  responsive={ false }>
-                { options.map(((color)=> {
-                  const isColorDisabled = disabledOptions.indexOf(color) !== -1;
-                  return (
-                    <GridCell key={ color } shrink>
-                      <ColorPickerOption
-                          color={ color }
-                          data-ax-at={ atIds.ColorPicker.option }
-                          disabled={ isColorDisabled }
-                          onClick={ () => isColorDisabled || onSelectColor(color) } />
-                    </GridCell>
-                  );
-                } ) ) }
-              </Grid>
-            </DropdownContent>
-          </DropdownContext>
-        </DropdownSource>
-      </Dropdown>
-    );
-  }
+      <DropdownSource>
+        <DropdownContext width={ width }>
+          <DropdownContent>
+            <Grid gutters="small" horizontalAlign="middle" responsive={ false }>
+              { options.map(color => {
+                const isColorDisabled = disabledOptions.indexOf(color) !== -1;
+                return (
+                  <GridCell key={ color } shrink>
+                    <ColorPickerOption
+                        color={ color }
+                        data-ax-at={ atIds.ColorPicker.option }
+                        disabled={ isColorDisabled }
+                        onClick={ () => isColorDisabled || onSelectColor(color) }/>
+                  </GridCell>
+                );
+              }) }
+            </Grid>
+          </DropdownContent>
+        </DropdownContext>
+      </DropdownSource>
+    </Dropdown>
+  );
 }
+
+ColorPicker.propTypes = {
+  /** Colors that should be disabled from selection */
+  disabledOptions: PropTypes.arrayOf(
+    PropTypes.oneOf([
+      'forbidden-planet',
+      'tiny-clanger',
+      'critical-mass',
+      'fantastic-voyage',
+      'paradise-lost',
+      'serene-sea',
+      'event-horizon',
+      'electric-dreams',
+      'outer-limits',
+      'giant-leap',
+      'moon-lagoon',
+      'space-invader',
+      'extraterrestrial',
+      'terra-form',
+      'primeval-soup',
+      'future-shock',
+      'sun-maker',
+      'new-horizon',
+      'blast-off',
+      'crash-course',
+      'solar-rust',
+      'ground-control',
+      'space-oddity',
+      'rocky-planet',
+      'deep-thought',
+      'luna-dust',
+    ])
+  ),
+  /** Callback for when the color picker has been opened */
+  onOpen: PropTypes.func,
+  /** Callback for when a color has been selected, provided with the selected color */
+  onSelectColor: PropTypes.func.isRequired,
+  /** Colors that should be visible for selection */
+  options: PropTypes.arrayOf(
+    PropTypes.oneOf([
+      'forbidden-planet',
+      'tiny-clanger',
+      'critical-mass',
+      'fantastic-voyage',
+      'paradise-lost',
+      'serene-sea',
+      'event-horizon',
+      'electric-dreams',
+      'outer-limits',
+      'giant-leap',
+      'moon-lagoon',
+      'space-invader',
+      'extraterrestrial',
+      'terra-form',
+      'primeval-soup',
+      'future-shock',
+      'sun-maker',
+      'new-horizon',
+      'blast-off',
+      'crash-course',
+      'solar-rust',
+      'ground-control',
+      'space-oddity',
+      'rocky-planet',
+      'deep-thought',
+      'luna-dust',
+    ])
+  ),
+  /** Color that should appear as selected */
+  selected: PropTypes.oneOf([
+    'forbidden-planet',
+    'tiny-clanger',
+    'critical-mass',
+    'fantastic-voyage',
+    'paradise-lost',
+    'serene-sea',
+    'event-horizon',
+    'electric-dreams',
+    'outer-limits',
+    'giant-leap',
+    'moon-lagoon',
+    'space-invader',
+    'extraterrestrial',
+    'terra-form',
+    'primeval-soup',
+    'future-shock',
+    'sun-maker',
+    'new-horizon',
+    'blast-off',
+    'crash-course',
+    'solar-rust',
+    'ground-control',
+    'space-oddity',
+    'rocky-planet',
+    'deep-thought',
+    'luna-dust',
+  ]),
+  /** Diameter of the color picker button */
+  size: PropTypes.string,
+  /** Width of the color selection container */
+  width: PropTypes.string,
+};
