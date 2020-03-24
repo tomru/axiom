673e6afdd6f742a38431ff2aafad588ecdcd2e2c
laurie <laurie@brandwatch.com>
2020-03-24 15:29:08 +0000

    docs: ✏️ Tip, adds basic example

---
+++ packages/axiom-components/src/Tip/Tip.stories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Tip from './Tip';
+import AlertCard from '../AlertCard/AlertCard';
+import Base from '../Base/Base';
+
+export default {
+  title: 'Components/Tip',
+  component: Tip,
+};
+
+export function Default() {
+  return (
+    <Base container>
+      <Tip color="shade-2" shadow={ false } size="medium">
+        <AlertCard onRemoveClick={ () => {} } shade="shade-2" space="x2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit
+          amet pulvinar lorem. Aliquam porta eros at dui commodo posuere.
+        </AlertCard>
+      </Tip>
+    </Base>
+  );
+}
