68051ec2b2aed57a2d6a1658c74275a767663a95
laurie <laurie@brandwatch.com>
2020-03-24 15:12:09 +0000

    docs: ✏️ Storybook, add knobs add on
    Allows interactive stories, prop manipulation.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../packages/**/*.stories.(js|mdx)'],
-  addons: ['@storybook/addon-docs'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-knobs/register'],
 };
