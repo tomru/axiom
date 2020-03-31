204582926263b348d9256f224235f5c0c16276ad
laurie <laurie@brandwatch.com>
2020-03-31 17:38:25 +0100

    docs: ✏️ Set text and background color to be Axiom CSS vars

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
 
-addDecorator(storyFn => <div className="ax-theme--day">{storyFn()}</div>);
+addDecorator(storyFn => (
+  <div
+      className="ax-theme--day"
+      style={ {
+        backgroundColor: 'var(--color-theme-background)',
+        color: 'var(--color-theme-text)',
+      } }>
+    { storyFn() }
+  </div>
+));
