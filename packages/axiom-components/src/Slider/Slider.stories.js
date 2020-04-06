96603da62e7ffff66d77bea0db1723ea877e2c97
laurie <laurie@brandwatch.com>
2020-04-06 18:41:28 +0100

    docs: ✏️ Slider add story

---
+++ packages/axiom-components/src/Slider/Slider.stories.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Slider from "./Slider";
+
+export default {
+  title: "Components/Slider",
+  component: Slider,
+};
+
+export function Default() {
+  const [value, setValue] = useState(50);
+
+  return (
+    <Slider
+      max={200}
+      min={0}
+      onChange={(value) => setValue(value)}
+      size="small"
+      value={value}
+    />
+  );
+}
