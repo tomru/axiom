da220ba37a58187751502d50fb0fbe2ecae80f4f
laurie <laurie@brandwatch.com>
2020-04-06 19:49:01 +0100

    docs: ✏️ StatusBadge

---
+++ packages/axiom-components/src/StatusBadge/StatusBadge.stories.js
@@ -0,0 +1,31 @@
+import React from "react";
+import StatusBadge from "./StatusBadge";
+import Grid from "../Grid/Grid";
+import GridCell from "../Grid/GridCell";
+
+export default {
+  title: "Components/StatusBadge",
+  component: StatusBadge,
+};
+
+export function Default() {
+  return (
+    <Grid gutters="tiny">
+      <GridCell none>
+        <StatusBadge status="alpha" />
+      </GridCell>
+      <GridCell none>
+        <StatusBadge status="beta" />
+      </GridCell>
+      <GridCell none>
+        <StatusBadge status="internal" />
+      </GridCell>
+      <GridCell none>
+        <StatusBadge status="new" />
+      </GridCell>
+      <GridCell none>
+        <StatusBadge status="prototype" />
+      </GridCell>
+    </Grid>
+  );
+}
