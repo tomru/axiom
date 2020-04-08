69bcb579d28cb363c54afbd74c4a10dd59f8c7f8
laurie <laurie@brandwatch.com>
2020-04-08 19:03:38 +0100

    docs: ✏️ Separator

---
+++ packages/axiom-components/src/Separator/Separator.stories.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Separator from "./Separator";
+import { withKnobs, select } from "@storybook/addon-knobs";
+
+export default {
+  title: "Components/Separator",
+  component: Separator,
+  decorators: [
+    withKnobs,
+    (storyFn) => (
+      <div
+        style={{
+          height: "100px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {storyFn()}
+      </div>
+    ),
+  ],
+};
+
+export function Default() {
+  return (
+    <Separator
+      direction={select("direction", ["horizontal", "vertical"], "horizontal")}
+      borderStyle={select("borderStyle", ["solid", "dotted"], "solid")}
+    />
+  );
+}
