606b690bd3e440fe261dc50557aacc104f03d3d8
laurie <laurie@brandwatch.com>
2020-04-09 16:26:47 +0100

    docs: ✏️ Transition

---
+++ packages/axiom-components/src/Transition/Transition.stories.js
@@ -0,0 +1,66 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
+import Transition from "./Transition";
+import Button from "../Button/Button";
+import Icon from "../Icon/Icon";
+import "./Transition.stories.css";
+
+export default {
+  title: "Components/Transition",
+  component: Transition,
+  decorators: [(storyFn) => <WrapperComponet storyFn={storyFn} />],
+};
+
+function WrapperComponet({ storyFn }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return (
+    <div>
+      {storyFn({ activeIndex })}
+      <div className="controls-wrapper">
+        <Button
+          disabled={activeIndex === 0}
+          onClick={() => setActiveIndex((i) => i - 1)}
+          shape="circle"
+          size="small"
+          style="secondary"
+        >
+          <Icon name="chevron-left" />
+        </Button>
+
+        <Button
+          disabled={activeIndex === 4}
+          onClick={() => setActiveIndex((i) => i + 1)}
+          shape="circle"
+          size="small"
+          style="secondary"
+        >
+          <Icon name="chevron-right" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+function ColorBox({ color }) {
+  return (
+    <div
+      style={{
+        height: "50px",
+        backgroundColor: `var(--color-product-${color})`,
+      }}
+    />
+  );
+}
+
+export function Default({ activeIndex }) {
+  return (
+    <Transition activeIndex={activeIndex} height="50px" width="150px">
+      <ColorBox color="paradise-lost" />
+      <ColorBox color="moon-lagoon" />
+      <ColorBox color="forbidden-planet" />
+      <ColorBox color="sun-maker" />
+      <ColorBox color="future-shock" />
+    </Transition>
+  );
+}
