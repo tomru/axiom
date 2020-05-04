32efbf3abe4d5c49b7f5ec3cad10b2931c677960
laurie <laurie@brandwatch.com>
2020-05-04 18:14:30 +0100

    ci: 🎡 Split out CJS build
    We run in this in it's own job to ensure that the build is passing. We
    should be able to drop cjs support once node fully supports esm modules

---
+++ scripts/build_cjs_packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Prepare
+npx lerna exec --parallel -- rm -rf dist
+
+## CJS
+npx lerna exec --parallel -- mkdir -p "dist/cjs"
+npx lerna exec --parallel -- rsync -a --prune-empty-dirs --exclude '__snapshots__/*' --exclude '*.test.js' src/* dist/cjs
+npx lerna exec --parallel -- BABEL_ENV=cjs npx babel dist/cjs -d dist/cjs --config-file $(pwd)/babel.config.js
+
+# Post CSS
+npx postcss 'packages/**/dist/**/*.css' --config ./postcss.prod.config.js --replace --verbose
