cf10c2f8beb06c9196f2ffa3cc5be958f869564e
laurie <laurie@brandwatch.com>
2020-05-13 17:00:37 +0100

    chore(lint): Make commit lint a little more forgiving
    I dont really see a problem with fullstops and Uppercase subject line.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [0, "never", 0],
+    "subject-full-stop": [0, "never", 0],
+  },
+};
