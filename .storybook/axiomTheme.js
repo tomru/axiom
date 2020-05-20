5412a1201f6fa8763583e59b35b4c92418bf1461
laurie <laurie@brandwatch.com>
2020-05-20 16:34:26 +0100

    docs: ✏️ adds axiom colors to a sb theme
    using axiom css vars is tricky so leaving for now

--- .storybook/axiomTheme.js
+++ .storybook/axiomTheme.js
@@ -1,6 +1,44 @@
-import { create } from '@storybook/theming/create';
+import { create } from "@storybook/theming/create";
+import logo from "./static_assets/axiom-text.svg";
+
+const borderColor = "rgba(63,63,63, 0.2)"; // --color-theme-day-border: rgba(var(--rgb-theme-day-main), var(--opacity-border));
+const active = "#239EDB"; // --color-ui-accent--active;
+const white = "#FFFFFF"; // --rgb-ui-white:
+const whiteNoiseDefault = "#F8F8F8"; // --rgb-ui-white-noise
+const carbonDefault = "#3F3F3F"; // --rgb-ui-carbon
+const componentBorderRadius = "0.1875rem"; // --component-border-radius;
 
 export default create({
-  brandTitle: 'Axiom',
-  brandUrl: 'https://axiom.brandwatch.com/',
+  base: "light", // base to extend I guess this is SB's light theme
+
+  brandTitle: "Axiom",
+  brandUrl: "https://github.com/BrandwatchLtd/axiom-react",
+  brandImage: logo,
+  //colorPrimary: "palegoldenrod", // Not sure where this is used
+  colorSecondary: active, // active used in side bar for active story
+
+  //UI
+  appBg: whiteNoiseDefault,
+  appContentBg: white,
+  appBorderColor: borderColor,
+  appBorderRadius: componentBorderRadius,
+
+  // Typography
+  fontBase: "Roboto",
+  fontCode: "Monaco", // font used for code samples
+
+  // Text colors
+  textColor: carbonDefault,
+  //textInverseColor: "pink", // Used in Action Logger and notifications
+
+  // Toolbar default and active colors
+  barTextColor: carbonDefault,
+  barSelectedColor: active, //active
+  barBg: white,
+
+  // Form colors
+  inputBg: white,
+  inputBorder: borderColor,
+  inputTextColor: carbonDefault,
+  inputBorderRadius: componentBorderRadius,
 });
