24d27617ea9d3d685f0e2486b159cb83f6d7e36d
laurie <laurie@brandwatch.com>
2020-06-07 13:30:40 +0100

    docs: ✏️ Show canvas tab on site build
    Hiding canvas also hides the theme button and the addons

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,15 +1,6 @@
 import { addons } from "@storybook/addons";
 import axiomTheme from "./axiomTheme";
 
-let config = { theme: axiomTheme };
-
-if (NODE_ENV === "production") {
-  config = {
-    ...config,
-    previewTabs: {
-      canvas: { hidden: true },
-    },
-  };
-}
-
-addons.setConfig(config);
+addons.setConfig({
+  theme: axiomTheme,
+});
