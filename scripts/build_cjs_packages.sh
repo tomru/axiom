2e9db47952c66318f3277f3a9411a07e2e239eaa
laurie <laurie@brandwatch.com>
2020-07-08 13:30:03 +0100

    ci: 🎡 Exclude stories from packages build
    Storybook was picking up to copies of each story

--- scripts/build_cjs_packages.sh
+++ scripts/build_cjs_packages.sh
@@ -6,7 +6,7 @@ npx lerna exec --parallel -- rm -rf dist
 
 ## CJS
 npx lerna exec --parallel -- mkdir -p "dist/cjs"
-npx lerna exec --parallel -- rsync -a --prune-empty-dirs --exclude '__snapshots__/*' --exclude '*.test.js' src/* dist/cjs
+npx lerna exec --parallel -- rsync -a --prune-empty-dirs --exclude '__snapshots__/*' --exclude '*.test.js' --exclude '*.stories.js' src/* dist/cjs
 npx lerna exec --parallel -- BABEL_ENV=cjs npx babel dist/cjs -d dist/cjs --config-file $(pwd)/babel.config.js
 
 # Post CSS
--- scripts/build_esm_packages.sh
+++ scripts/build_esm_packages.sh
@@ -6,7 +6,7 @@ npx lerna exec --parallel -- rm -rf dist
 
 ## ESM
 npx lerna exec --parallel -- mkdir -p "dist/esm"
-npx lerna exec --parallel -- rsync -a --prune-empty-dirs --exclude '__snapshots__/*' --exclude '*.test.js' src/* dist/esm
+npx lerna exec --parallel -- rsync -a --prune-empty-dirs --exclude '__snapshots__/*' --exclude '*.test.js' --exclude '*.stories.js' --exclude '*.stories.mdx' src/* dist/esm
 npx lerna exec --parallel -- npx babel dist/esm -d dist/esm --config-file $(pwd)/babel.config.js
 
 # Post CSS
