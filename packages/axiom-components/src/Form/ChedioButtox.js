6e00d7681522cb5a19c7668202ae00c3786dbc32
laurie <laurie@brandwatch.com>
2020-07-29 15:42:42 +0100

    fix: 🐛 Checkbox indeterminate style

--- packages/axiom-components/src/Form/ChedioButtox.js
+++ packages/axiom-components/src/Form/ChedioButtox.js
@@ -3,73 +3,80 @@ import React, { Component } from "react";
 import classnames from "classnames";
 import Base from "../Base/Base";
 import Tick from "./Tick";
+import Indeterminate from "./Indeterminate";
 
 import "./ChedioButtox.css";
 
-export default class ChedioButtox extends Component {
-  static propTypes = {
-    checked: PropTypes.bool,
-    children: PropTypes.node,
-    className: PropTypes.string.isRequired,
-    disabled: PropTypes.bool,
-    indeterminate: PropTypes.bool,
-    inputType: PropTypes.oneOf(["checkbox", "radio"]).isRequired,
-    invalid: PropTypes.bool,
-    onClick: PropTypes.func,
-    title: PropTypes.string,
+export default function ChedioButtox({
+  children,
+  className,
+  disabled,
+  inputType,
+  invalid,
+  title,
+  onClick,
+  indeterminate,
+  checked,
+  ...rest
+}) {
+  const getMarker = () => {
+    if (indeterminate) {
+      return <Indeterminate />;
+    }
+
+    if (checked) {
+      return <Tick />;
+    }
   };
 
-  render() {
-    const {
-      children,
-      className,
-      disabled,
-      inputType,
-      invalid,
-      title,
-      onClick,
-      indeterminate,
-      checked,
-      ...rest
-    } = this.props;
-
-    const classes = classnames(className, {
-      [`${className}--disabled`]: disabled,
-      [`${className}--invalid`]: invalid,
-      [`${className}--indeterminate`]: indeterminate,
+  const handleClick =
+    onClick &&
+    ((e) => {
+      if (e.target.tagName !== "INPUT") {
+        e.stopPropagation();
+        return;
+      }
+      onClick(e);
     });
 
-    const handleClick =
-      onClick &&
-      ((e) => {
-        if (e.target.tagName !== "INPUT") {
-          e.stopPropagation();
-          return;
-        }
-        onClick(e);
-      });
+  const classes = classnames(className, {
+    [`${className}--disabled`]: disabled,
+    [`${className}--invalid`]: invalid,
+    [`${className}--indeterminate`]: indeterminate,
+  });
 
-    return (
-      <Base
-        Component="label"
-        className={classes}
-        onClick={handleClick}
-        space="x2"
-        title={title}
-      >
-        <input
-          {...rest}
-          checked={checked}
-          className={`${className}__input`}
-          disabled={disabled}
-          type={inputType}
-        />
+  return (
+    <Base
+      Component="label"
+      className={classes}
+      onClick={handleClick}
+      space="x2"
+      title={title}
+    >
+      <input
+        {...rest}
+        checked={checked}
+        className={`${className}__input`}
+        disabled={disabled}
+        type={inputType}
+      />
 
-        <span className={`${className}__indicator`}>
-          {inputType === "checkbox" && checked && <Tick />}
-        </span>
-        {children && <span className={`${className}__label`}>{children}</span>}
-      </Base>
-    );
-  }
+      <span className={`${className}__indicator`}>
+        {inputType === "checkbox" && getMarker()}
+      </span>
+      {children && <span className={`${className}__label`}>{children}</span>}
+    </Base>
+  );
 }
+
+ChedioButtox.propTypes = {
+  checked: PropTypes.bool,
+  children: PropTypes.node,
+  className: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+  indeterminate: PropTypes.bool,
+  inputType: PropTypes.oneOf(["checkbox", "radio"]).isRequired,
+  invalid: PropTypes.bool,
+  onClick: PropTypes.func,
+  title: PropTypes.string,
+};
---
+++ packages/axiom-components/src/Form/Indeterminate.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Indeterminate() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 16 16"
+    >
+      <path
+        className="indeterminate"
+        fill="white"
+        fillRule="evenodd"
+        d="M3 7h10v2H3z"
+      />
+    </svg>
+  );
+}
