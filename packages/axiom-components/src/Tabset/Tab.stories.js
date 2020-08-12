b8fb56550e55e406948c466115e1bb116f1476ca
laurie <laurie@brandwatch.com>
2020-08-12 18:10:44 +0100

    docs: ✏️ Adds story for Tabset/Tab
    Closes #929

---
+++ packages/axiom-components/src/Tabset/Tab.stories.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Tabset from "./Tabset";
+import Tab from "./Tab";
+
+export default {
+  title: "Tab",
+  component: Tab,
+};
+
+export function Default(props) {
+  return (
+    <Tabset>
+      <Tab title="Tab 1" {...props}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mauris
+        libero, suscipit eu dignissim eu, consectetur in mauris. Donec semper
+        tristique ipsum non vehicula. In eget libero lobortis, sollicitudin
+        velit nec, accumsan dolor. Morbi in finibus mauris, id pretium ipsum.
+        Sed quis massa tempus nunc molestie ultrices sit amet vel urna.
+      </Tab>
+      <Tab title="Tab 2" {...props}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mauris
+        libero, suscipit eu dignissim eu, consectetur in mauris. Donec semper
+        tristique ipsum non vehicula. In eget libero lobortis, sollicitudin
+        velit nec, accumsan dolor. Morbi in finibus mauris, id pretium ipsum.
+        Sed quis massa tempus nunc molestie ultrices sit amet vel urna.
+      </Tab>
+      <Tab title="Tab 3" {...props}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mauris
+        libero, suscipit eu dignissim eu, consectetur in mauris. Donec semper
+        tristique ipsum non vehicula. In eget libero lobortis, sollicitudin
+        velit nec, accumsan dolor. Morbi in finibus mauris, id pretium ipsum.
+        Sed quis massa tempus nunc molestie ultrices sit amet vel urna.
+      </Tab>
+    </Tabset>
+  );
+}
---
+++ packages/axiom-components/src/Tabset/Tabset.stories.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Tab from "./Tab";
+import Tabset from "./Tabset";
+
+export default {
+  title: "Tabset",
+  component: Tabset,
+  subcomponents: { Tab },
+};
+
+export function Default({ size, space, activeTabIndex }) {
+  return (
+    <Tabset size={size} space={space} activeTabIndex={activeTabIndex}>
+      <Tab title="Tab 1">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mauris
+        libero, suscipit eu dignissim eu, consectetur in mauris. Donec semper
+        tristique ipsum non vehicula. In eget libero lobortis, sollicitudin
+        velit nec, accumsan dolor. Morbi in finibus mauris, id pretium ipsum.
+        Sed quis massa tempus nunc molestie ultrices sit amet vel urna.
+      </Tab>
+      <Tab title="Tab 2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mauris
+        libero, suscipit eu dignissim eu, consectetur in mauris. Donec semper
+        tristique ipsum non vehicula. In eget libero lobortis, sollicitudin
+        velit nec, accumsan dolor. Morbi in finibus mauris, id pretium ipsum.
+        Sed quis massa tempus nunc molestie ultrices sit amet vel urna.
+      </Tab>
+      <Tab title="Tab 3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mauris
+        libero, suscipit eu dignissim eu, consectetur in mauris. Donec semper
+        tristique ipsum non vehicula. In eget libero lobortis, sollicitudin
+        velit nec, accumsan dolor. Morbi in finibus mauris, id pretium ipsum.
+        Sed quis massa tempus nunc molestie ultrices sit amet vel urna.
+      </Tab>
+    </Tabset>
+  );
+}
