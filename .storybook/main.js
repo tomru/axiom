99ed0eea6bc8afbfee656d56fb8094cae449314c
laurie <laurie@brandwatch.com>
2020-08-13 17:08:15 +0100

    docs: Fix ally storybook add on

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: ["../packages/**/*.stories.@(mdx|js)"],
   addons: [
     "@storybook/addon-docs",
-    "@storybook/addon-a11y/register",
+    "@storybook/addon-a11y",
     "@storybook/addon-controls",
     "@storybook/addon-toolbars",
   ],
