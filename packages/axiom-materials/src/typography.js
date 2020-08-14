925ae17af4d9efe8def26ad878a26431f291627e
laurie <laurie@brandwatch.com>
2020-08-14 16:31:22 +0100

    feat: 🎸 Provide a js export for typography values
    closes #944

---
+++ packages/axiom-materials/src/typography.js
@@ -0,0 +1,28 @@
+export const fontFamilyBody = ["Roboto", "Helvetica", "Arial", "sansserif"];
+
+export const fontWeightThin = 100;
+export const fontWeightLight = 300;
+export const fontWeightRegular = 400;
+export const fontWeightMedium = 500;
+export const fontWeightBold = 700;
+
+export const fontSizeLabel = "0.625rem";
+export const fontSizeSmall = "0.75rem";
+export const fontSizeBody = "0.8125rem";
+export const fontSizeLarge = "0.875rem";
+export const fontSizeHeadtitle = "1.0625rem";
+export const fontSizeHeadline = "1.375rem";
+export const fontSizeDisplay1 = "2rem";
+export const fontSizeDisplay2 = "2.5rem";
+
+export const lineHeightLabel = "1rem";
+export const lineHeightBody = "1.25rem";
+export const lineHeightSmall = lineHeightBody;
+export const lineHeightLarge = lineHeightBody;
+export const lineHeightHeadtitle = "1.5rem";
+export const lineHeightHeadline = "1.75rem";
+export const lineHeightDisplay1 = "2.5rem";
+export const lineHeightDisplay2 = "3rem";
+
+export const letterSpacingTight = "-0.00125rem";
+export const letterSpacingLoose = "0.001875rem";
