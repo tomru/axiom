0e4ca48ea9f8a9060df943d083d9a24ee2120e67
laurie <laurie@brandwatch.com>
2020-09-14 11:26:31 +0100

    refactor: 💡 Remove legacy context API from DatePickerControls
    Convert to function component to make use of useContext

--- packages/axiom-components/src/DatePicker/DatePickerControls.js
+++ packages/axiom-components/src/DatePicker/DatePickerControls.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { mediumDate } from "@brandwatch/axiom-formatting";
 import Button from "../Button/Button";
 import ButtonGroup from "../Button/ButtonGroup";
@@ -7,71 +7,59 @@ import Grid from "../Grid/Grid";
 import GridCell from "../Grid/GridCell";
 import DropdownReactContext from "../Dropdown/DropdownReactContext";
 
-export default class DatePickerControls extends Component {
-  static propTypes = {
-    onApply: PropTypes.func,
-    onCancel: PropTypes.func,
-    rangeSelect: PropTypes.bool,
-    selectedDate: PropTypes.instanceOf(Date),
-    selectedEndDate: PropTypes.instanceOf(Date),
-    selectedStartDate: PropTypes.instanceOf(Date),
-    view: PropTypes.string.isRequired,
-  };
+export default function DatePickerControls({
+  onCancel,
+  onApply,
+  rangeSelect,
+  selectedDate,
+  selectedEndDate,
+  selectedStartDate,
+  view,
+}) {
+  const { closeDropdown } = useContext(DropdownReactContext);
 
-  static contextType = DropdownReactContext;
-
-  constructor(props) {
-    super(props);
-    this.handleApply = this.handleApply.bind(this);
-    this.handleCancel = this.handleCancel.bind(this);
+  function handleApply() {
+    onApply && onApply();
+    closeDropdown();
   }
 
-  handleApply() {
-    this.props.onApply && this.props.onApply();
-    this.context.closeDropdown();
+  function handleCancel() {
+    onCancel && onCancel();
+    closeDropdown();
   }
 
-  handleCancel() {
-    this.props.onCancel && this.props.onCancel();
-    this.context.closeDropdown();
-  }
-
-  render() {
-    const {
-      rangeSelect,
-      selectedDate,
-      selectedEndDate,
-      selectedStartDate,
-      view,
-    } = this.props;
-
-    return (
-      <Grid horizontalAlign="end" responsive={false}>
-        {view === "double" && (
-          <GridCell hiddenUntil="small">
-            {rangeSelect && selectedStartDate && mediumDate(selectedStartDate)}
-            {rangeSelect &&
-              selectedEndDate &&
-              ` – ${mediumDate(selectedEndDate)}`}
-            {!rangeSelect && selectedDate && mediumDate(selectedDate)}
-          </GridCell>
-        )}
-
-        <GridCell shrink>
-          <ButtonGroup>
-            <Button onClick={this.handleApply} size="small" variant="primary">
-              Apply
-            </Button>
-            <Button
-              onClick={this.handleCancel}
-              size="small"
-              variant="secondary"
-            >
-              Cancel
-            </Button>
-          </ButtonGroup>
+  return (
+    <Grid horizontalAlign="end" responsive={false}>
+      {view === "double" && (
+        <GridCell hiddenUntil="small">
+          {rangeSelect && selectedStartDate && mediumDate(selectedStartDate)}
+          {rangeSelect &&
+            selectedEndDate &&
+            ` – ${mediumDate(selectedEndDate)}`}
+          {!rangeSelect && selectedDate && mediumDate(selectedDate)}
         </GridCell>
-      </Grid>
-    );
-  }
+      )}
+
+      <GridCell shrink>
+        <ButtonGroup>
+          <Button onClick={handleApply} size="small" variant="primary">
+            Apply
+          </Button>
+          <Button onClick={handleCancel} size="small" variant="secondary">
+            Cancel
+          </Button>
+        </ButtonGroup>
+      </GridCell>
+    </Grid>
+  );
 }
+
+DatePickerControls.propTypes = {
+  onApply: PropTypes.func,
+  onCancel: PropTypes.func,
+  rangeSelect: PropTypes.bool,
+  selectedDate: PropTypes.instanceOf(Date),
+  selectedEndDate: PropTypes.instanceOf(Date),
+  selectedStartDate: PropTypes.instanceOf(Date),
+  view: PropTypes.string.isRequired,
+};
